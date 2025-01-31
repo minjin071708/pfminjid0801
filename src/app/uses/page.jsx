1c2113c1d95736d466fbfee90642f2dc35dc61c3
minjin071708 <96518262+minjin071708@users.noreply.github.com>
2025-01-31 19:47:19 +0900

    Update page.jsx

--- src/app/uses/page.jsx
+++ src/app/uses/page.jsx
@@ -35,7 +35,16 @@ export default function Uses() {
       intro="“Sigan con el buen trabajo.” (keep up the good work)"
     >
       <div className="space-y-20">
-        <ToolsSection title="2023.01.02 ~ 재직중 ">
+        <ToolsSection title="2024.12.11 ~ 재직중 ">
+          <Tool title="새 스타트업 IT회사">
+          현대 건설의 스마트 안전교육 시스템 구축
+          프로젝트의 디자인, 퍼블리싱 담당
+          </Tool>
+          
+          
+    
+        </ToolsSection>
+        <ToolsSection title="2023.01.02 ~ 2024.11.15 ">
           <Tool title="테크월드 메디어 회사">
           웹디자인, 프론트엔드, 웹페이지 관리
           이직사유 : 지금 업무가 다지인 위주라서 그거 보다 개발을 하고 싶어요
